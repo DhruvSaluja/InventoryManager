b3e4729a20232329575c98309cef0b7eadace8bc
Dhruv Saluja <dhruvsaluja04@gmail.com>
2024-08-08 14:43:49 +0530

    api_key_hiding

--- firebase.js
+++ firebase.js
@@ -3,18 +3,19 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth"
+import apiKey from "./config"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB-yNIygKvJvSD9sERHp13fxB4ItZ3Kta0",
+  apiKey: apiKey.api_Key,
   authDomain: "inventory-management-76c35.firebaseapp.com",
   projectId: "inventory-management-76c35",
   storageBucket: "inventory-management-76c35.appspot.com",
-  messagingSenderId: "865716784614",
-  appId: "1:865716784614:web:6e60444d66dadf81211a27",
+  messagingSenderId:apiKey.messaging_SenderId,
+  appId:apiKey.app_Id ,
   measurementId: "G-FHBTGJDCLB"
 };
 
