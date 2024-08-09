6b4e1c4cd15f6e6973730483fda874e02cff28b9
Dhruv Saluja <dhruvsaluja04@gmail.com>
2024-08-09 18:47:53 +0530

    created env file

--- firebase.js
+++ firebase.js
@@ -3,19 +3,19 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth"
-import apiKey from "./config.js"
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: apiKey.api_Key,
+  apiKey: process.env.api_Key,
   authDomain: "inventory-management-76c35.firebaseapp.com",
   projectId: "inventory-management-76c35",
   storageBucket: "inventory-management-76c35.appspot.com",
-  messagingSenderId:apiKey.messaging_SenderId,
-  appId:apiKey.app_Id ,
+  messagingSenderId:process.env.messaging_SenderId,
+  appId:process.env.app_Id ,
   measurementId: "G-FHBTGJDCLB"
 };
 
