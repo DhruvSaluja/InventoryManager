bdb4c2efad37a21eacb8d3e8d24d6f8e5bdb0d40
Dhruv Saluja <dhruvsaluja04@gmail.com>
2024-08-09 15:35:13 +0530

    updating config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const apikey = {
-    api_Key: "AIzaSyB-yNIygKvJvSD9sERHp13fxB4ItZ3Kta0",
-    messaging_SenderId: "865716784614",
-    app_Id: "1:865716784614:web:6e60444d66dadf81211a27"
-}
-
-export default apikey
